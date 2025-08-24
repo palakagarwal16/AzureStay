fe859fe231a7581c45f5eaaf1abe0b6148791619
palakagarwal16 <palakagarwal1635@gmail.com>
2025-08-24 22:31:55 +0530

    update dashboard

--- src/hotelmanagement/Dashboard.java
+++ src/hotelmanagement/Dashboard.java
@@ -14,6 +14,38 @@ public class Dashboard extends JFrame{
         image.setBounds(0,0,1550, 1000);
         add(image);
 
+        JLabel Text  = new JLabel("Welcome to the Luxury space in !!");
+        Text.setBounds(400,80,1050, 50);
+        Text.setFont(new Font("Tahoma" ,Font.PLAIN , 46));
+        Text.setForeground(Color.WHITE);
+        image.add(Text);
+
+
+        JMenuBar mb = new JMenuBar();
+        mb.setBounds( 0 ,0 , 1550 , 30 );
+        image.add(mb);
+
+        JMenu hotel = new JMenu( "Hotel Management");
+        hotel.setForeground(Color.BLACK);
+        mb.add(hotel);
+
+        JMenu Admin = new JMenu("Admin");
+        Admin.setForeground(Color.BLACK);
+        mb.add(Admin);
+
+        JMenuItem Reception = new JMenuItem("Reception");
+        hotel.add(Reception);
+
+        JMenuItem AddROOMS = new JMenuItem("Add Rooms");
+        Admin.add(AddROOMS);
+
+        JMenuItem AddDRIVERS = new JMenuItem("Add Drivers");
+        Admin.add(AddDRIVERS);
+
+        JMenuItem Addemployee = new JMenuItem("Add Employee");
+        Admin.add(Addemployee);
+
+
 
         setVisible(true);
     }
